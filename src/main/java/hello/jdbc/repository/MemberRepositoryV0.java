2012bacaacf318c6d9d880182cc189f6b49d318c
songwonseock <tablier@gmail.com>
2022-07-31 23:49:36 +0900

    add original jdbc coding

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.member.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 /**
@@ -26,6 +27,9 @@ public class MemberRepositoryV0 {
       pstmt = con.prepareStatement(sql);
       pstmt.setString(1, member.getMemberId());
       pstmt.setInt(2, member.getMoney());
+
+      int saveCount = pstmt.executeUpdate();
+      log.info("saveCount={}", saveCount);
       return member;
     } catch (SQLException e) {
 
@@ -37,7 +41,100 @@ public class MemberRepositoryV0 {
     }
   }
 
+  public Member findById(String memberId) throws SQLException {
+
+    String sql = "select * From member where member_id = ?";
+
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+
+    try {
+
+      con = getConnection();
+      pstmt = con.prepareStatement(sql);
+      pstmt.setString(1, memberId);
+
+      rs = pstmt.executeQuery();
+      if (rs.next()) {
+        Member member = new Member();
+        member.setMemberId(rs.getString("member_id"));
+        member.setMoney(rs.getInt("money"));
+        return member;
+      } else {
+        throw new NoSuchElementException("member not found memberId=" + memberId);
+      }
+
+    } catch (SQLException e) {
+
+      log.error("db error", e);
+      throw e;
+    } finally {
+
+      close(con, pstmt, rs);
+    }
+  }
+
+  public void update(String memberId, int money) throws SQLException {
+
+    String sql = "update member set money=? where member_id=?";
+
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+
+    try {
+
+      con = getConnection();
+      pstmt = con.prepareStatement(sql);
+      pstmt.setInt(1, money);
+      pstmt.setString(2, memberId);
+      int updateResultSize = pstmt.executeUpdate();
+      log.info("updateResultSize={}", updateResultSize);
+    } catch (SQLException e) {
+
+      log.error("db error", e);
+      throw e;
+    } finally {
+
+      close(con, pstmt, rs);
+    }
+  }
+
+  public void delete(String memberId) throws SQLException {
+    String sql = "delete from member where member_id=?";
+
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+
+    try {
+
+      con = getConnection();
+      pstmt = con.prepareStatement(sql);
+      pstmt.setString(1, memberId);
+      int deleteResultSize = pstmt.executeUpdate();
+      log.info("deleteResultSize={}", deleteResultSize);
+    } catch (SQLException e) {
+
+      log.error("db error", e);
+      throw e;
+    } finally {
+
+      close(con, pstmt, rs);
+    }
+  }
+
   private void close(Connection con, Statement stmt, ResultSet rs) {
+
+    if (!Objects.isNull(rs)) {
+
+      try {
+        rs.close();
+      } catch (SQLException e) {
+        log.info("rs close error", e);
+      }
+    }
 
     if (!Objects.isNull(stmt)) {
 
@@ -48,21 +145,12 @@ public class MemberRepositoryV0 {
       }
     }
 
-    if (!Objects.isNull(rs)) {
-
-      try {
-        rs.close();
-      } catch (SQLException e) {
-        log.info("rs close error: {}", e);
-      }
-    }
-
     if (!Objects.isNull(con)) {
 
       try {
         con.close();
       } catch (SQLException e) {
-        log.info("con close error: {}", e);
+        log.info("con close error", e);
       }
     }
   }
--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -2,27 +2,39 @@ package hello.jdbc.repository;
 
 import hello.jdbc.member.Member;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 class MemberRepositoryV0Test {
 
-  private Member member;
-
-  @BeforeEach
-  void setUp() {
-    member = new Member("hi3", 30000);
-  }
+  MemberRepositoryV0 repository = new MemberRepositoryV0();
 
   @Test
-  void save() throws SQLException {
+  void crud() throws SQLException {
 
-    MemberRepositoryV0 v0 = new MemberRepositoryV0();
-    v0.save(member);
+    // save
+    Member member = new Member("memberV100", 40000);
+    repository.save(member);
+
+    // fineMember
+    Member findMember = repository.findById(member.getMemberId());
+    log.info("findMember={}", findMember);
+    assertEquals(member, findMember);
+
+    // update: money: 10000 -> 20000
+    repository.update(member.getMemberId(), 20000);
+    Member updatedMember = repository.findById(member.getMemberId());
+    assertEquals(20000, updatedMember.getMoney());
+
+    // delete
+    repository.delete(member.getMemberId());
+    assertThrows(NoSuchElementException.class, () -> repository.findById(member.getMemberId()));
   }
 }
