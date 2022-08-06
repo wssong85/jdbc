d981e28c0a835c60b53a209103a969904af74768
songwonseock <tablier@gmail.com>
2022-08-07 01:58:41 +0900

    spring sql error translate

--- src/main/java/hello/jdbc/repository/MemberRepositoryV4_1.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV4_1.java
@@ -1,6 +1,7 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.member.Member;
+import hello.jdbc.repository.ex.MyDbException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.jdbc.support.JdbcUtils;
@@ -10,20 +11,21 @@ import java.sql.*;
 import java.util.NoSuchElementException;
 
 /**
- * 트랜잭션 - 트랜잭션 매니저
- * DataSourceUtils.getConnection()
- * DataSourceUtils.releaseConnection()
+ * 예외 누수 문제 해결
+ * 체크 예외를 런타임 예외로 변경
+ * MemberRepository 인터페이스 사용
+ * 제거
  */
 @Slf4j
-public class MemberRepositoryV3 implements MemberRepositoryEx{
+public class MemberRepositoryV4_1 implements MemberRepository{
 
   private final DataSource dataSource;
 
-  public MemberRepositoryV3(DataSource dataSource) {
+  public MemberRepositoryV4_1(DataSource dataSource) {
     this.dataSource = dataSource;
   }
 
-  public Member save(Member member) throws SQLException {
+  public Member save(Member member) {
 
     String sql = "insert into member(member_id, money) values (?, ?)";
 
@@ -43,20 +45,24 @@ public class MemberRepositoryV3 implements MemberRepositoryEx{
     } catch (SQLException e) {
 
       log.error("db error", e);
-      throw e;
+      new MyDbException(e);
     } finally {
 
       close(con, pstmt, null);
     }
+
+    return member;
   }
 
-  public Member findById(String memberId) throws SQLException {
+  public Member findById(String memberId) {
 
     String sql = "select * From member where member_id = ?";
 
     Connection con = null;
     PreparedStatement pstmt = null;
     ResultSet rs = null;
+
+    Member member = null;
 
     try {
 
@@ -66,7 +72,7 @@ public class MemberRepositoryV3 implements MemberRepositoryEx{
 
       rs = pstmt.executeQuery();
       if (rs.next()) {
-        Member member = new Member();
+        member = new Member();
         member.setMemberId(rs.getString("member_id"));
         member.setMoney(rs.getInt("money"));
         return member;
@@ -77,14 +83,16 @@ public class MemberRepositoryV3 implements MemberRepositoryEx{
     } catch (SQLException e) {
 
       log.error("db error", e);
-      throw e;
+      new MyDbException(e);
     } finally {
 
       close(con, pstmt, rs);
     }
+
+    return member;
   }
 
-  public void update(String memberId, int money) throws SQLException {
+  public void update(String memberId, int money) {
 
     String sql = "update member set money=? where member_id=?";
 
@@ -103,14 +111,14 @@ public class MemberRepositoryV3 implements MemberRepositoryEx{
     } catch (SQLException e) {
 
       log.error("db error", e);
-      throw e;
+      new MyDbException(e);
     } finally {
 
       close(con, pstmt, rs);
     }
   }
 
-  public void delete(String memberId) throws SQLException {
+  public void delete(String memberId) {
     String sql = "delete from member where member_id=?";
 
     Connection con = null;
@@ -127,7 +135,7 @@ public class MemberRepositoryV3 implements MemberRepositoryEx{
     } catch (SQLException e) {
 
       log.error("db error", e);
-      throw e;
+      new MyDbException(e);
     } finally {
 
       close(con, pstmt, rs);
@@ -142,7 +150,7 @@ public class MemberRepositoryV3 implements MemberRepositoryEx{
     DataSourceUtils.releaseConnection(con, dataSource);
   }
 
-  private Connection getConnection() throws SQLException {
+  private Connection getConnection() {
 
     // 주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
     Connection con = DataSourceUtils.getConnection(dataSource);
---
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -0,0 +1,115 @@
+package hello.jdbc.service;
+
+import hello.jdbc.member.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import hello.jdbc.repository.MemberRepositoryV4_1;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * 트랜잭션 - DataSource, transactionManager 자동 등록
+ */
+@Slf4j
+@SpringBootTest
+class MemberServiceV4Test {
+
+  public static final String MEMBER_A = "memberA";
+  public static final String MEMBER_B = "memberB";
+  public static final String MEMBER_EX = "ex";
+
+  @Autowired
+  private MemberRepositoryV4_1 memberRepository;
+  @Autowired
+  private MemberServiceV4 memberService;
+
+  @RequiredArgsConstructor
+  @TestConfiguration
+  static class TestConfig {
+
+    private final DataSource dataSource;
+
+    @Bean
+    MemberRepositoryV4_1 memberRepository() {
+      return new MemberRepositoryV4_1(dataSource);
+    }
+
+    @Bean
+    MemberServiceV4 memberService() {
+      return new MemberServiceV4(memberRepository());
+    }
+  }
+
+  @Test
+  void AopCheck() {
+    log.info("memberService class={}", memberService.getClass());
+    log.info("memberRepository class={}", memberRepository.getClass());
+
+    Assertions.assertTrue(AopUtils.isAopProxy(memberService));
+    Assertions.assertFalse(AopUtils.isAopProxy(memberRepository));
+  }
+
+  @Test
+  @DisplayName("정상 이체")
+  void accountTransfer() {
+
+    //given
+    Member memberA = new Member(MEMBER_A, 10000);
+    Member memberB = new Member(MEMBER_B, 10000);
+    memberRepository.save(memberA);
+    memberRepository.save(memberB);
+
+    //when
+    memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+
+    //then
+    Member findMemberA = memberRepository.findById(memberA.getMemberId());
+    Member findMemberB = memberRepository.findById(memberB.getMemberId());
+
+    assertEquals(8000, findMemberA.getMoney());
+    assertEquals(12000, findMemberB.getMoney());
+  }
+
+  @Test
+  @DisplayName("이체중 예외 발생")
+  void accountTransferEx() {
+
+    //given
+    Member memberA = new Member(MEMBER_A, 10000);
+    Member memberB = new Member(MEMBER_EX, 10000);
+    memberRepository.save(memberA);
+    memberRepository.save(memberB);
+
+    //when
+    assertThrows(IllegalStateException.class,
+        () -> memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000));
+
+    //then
+    Member findMemberA = memberRepository.findById(memberA.getMemberId());
+    Member findMemberB = memberRepository.findById(memberB.getMemberId());
+
+    assertEquals(10000, findMemberA.getMoney());
+    assertEquals(10000, findMemberB.getMoney());
+  }
+
+  @AfterEach
+  void after() throws SQLException {
+    memberRepository.delete(MEMBER_A);
+    memberRepository.delete(MEMBER_B);
+    memberRepository.delete(MEMBER_EX);
+  }
+}
