6ff204ec1aa1e4cf6e4c36a912c56629daa76506
wssong85 <tabliss@naver.com>
2023-10-17 00:00:47 +0900

    main test1

--- src/main/java/hello/jdbc/connection/ConnectionConst.java
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -5,4 +5,5 @@ public class ConnectionConst {
   public static final String URL = "jdbc:h2:tcp://localhost:9092/~/test2";
   public static final String USERNAME = "sa";
   public static final String PASSWORD = "";
+
 }
